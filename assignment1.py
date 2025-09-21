0df1aac7e6ff80973c65857654715656b25420dc
Yuvraj <yuvrajdhiman17@gmail.com>
2025-09-21 21:16:47 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+
+#-----------TASK 1-----------------------
+
+a = int(input("First Number : "))
+b = int(input("Second Number : "))
+add = a+b
+sub = a-b
+multi = a*b
+div = a/b
+print("Addition : ", add)
+print("Subtraction : ",sub)
+print("Multiplication : " ,multi)
+print("Division : ", div)
+
+#--------------------TASK 2----------------------------
+
+a = input("Enter your first name : ")
+b = input("Enter your last name : ")
+c = a+" "+b
+print("HEllo," ,c ,"Welcome to the World." )
